caba7890d96911b4010ffc4fc0d171518270cd0f
Prince K <princejaj67@gmail.com>
2024-09-13 20:39:15 +0530

    Added floating can in clouds

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -19,76 +19,77 @@ gsap.registerPlugin(useGSAP, ScrollTrigger);
 export type HeroProps = SliceComponentProps<Content.HeroSlice>;
 
 const Hero = ({ slice }: HeroProps): JSX.Element => {
-
-  const ready=useStore(state=>state.ready);
+  const ready = useStore((state) => state.ready);
   const isDesktop = useMediaQuery("(min-width: 768px)", true);
 
-  useGSAP(() => {
-
-    if(!ready && isDesktop) return;
-    const introTl = gsap.timeline();
-    introTl
-      .set(".hero", { opacity: 1 })
-      .from(".hero-header-word", {
-        scale: 3,
-        opacity: 0,
-        ease: "power4.in",
-        delay: 0.3,
-        stagger: 1,
-      })
-      .from(
-        ".hero-subheading",
-        {
+  useGSAP(
+    () => {
+      if (!ready && isDesktop) return;
+      const introTl = gsap.timeline();
+      introTl
+        .set(".hero", { opacity: 1 })
+        .from(".hero-header-word", {
+          scale: 3,
           opacity: 0,
-          y: 30,
+          ease: "power4.in",
+          delay: 0.3,
+          stagger: 1,
+        })
+        .from(
+          ".hero-subheading",
+          {
+            opacity: 0,
+            y: 30,
+          },
+          "+=.8",
+        )
+        .from(".hero-body", {
+          opacity: 0,
+          y: 10,
+        })
+        .from(".hero-button", {
+          opacity: 0,
+          y: 10,
+          duration: 0.6,
+        });
+
+      const scrollTl = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".hero",
+          start: "top top",
+          end: "bottom bottom",
+          scrub: 1.5,
         },
-        "+=.8",
-      )
-      .from(".hero-body", {
-        opacity: 0,
-        y: 10,
-      })
-      .from(".hero-button", {
-        opacity: 0,
-        y: 10,
-        duration: 0.6,
       });
 
-    const scrollTl = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".hero",
-        start: "top top",
-        end: "bottom bottom",
-        scrub: 1.5,
-      },
-    });
-
-    scrollTl
-      .fromTo(
-        "body",
-        {
-          backgroundColor: "#FDE047",
-        },
-        {
-          backgroundColor: "#D9F99D",
-          overwrite: "auto",
-        },
-        1,
-      )
-      .from(".text-side-heading .split-char", {
-        scale: 1.3,
-        y: 40,
-        rotate: "-25",
-        opacity: 0,
-        stagger: 0.1,
-        ease: "back-out(3)",
-        duration: 0.5,
-      })
-      .from(".text-side-body", {
-        y: 20,
-        opacity: 0,
-      });
-  },{dependencies:[ready,isDesktop]});
+      scrollTl
+        .fromTo(
+          "body",
+          {
+            backgroundColor: "#FDE047",
+          },
+          {
+            backgroundColor: "#D9F99D",
+            overwrite: "auto",
+          },
+          1,
+        )
+        .from(".text-side-heading .split-char", {
+          scale: 1.3,
+          y: 40,
+          rotate: "-25",
+          opacity: 0,
+          stagger: 0.1,
+          ease: "back-out(3)",
+          duration: 0.5,
+        })
+        .from(".text-side-body", {
+          y: 20,
+          opacity: 0,
+        });
+    },
+    { dependencies: [ready, isDesktop] },
+  );
 
   return (
     <Bounded
@@ -97,12 +98,12 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       className="hero opacity-0"
     >
       {isDesktop && (
-      <View className="hero-scene pointer-events-none sticky top-0 z-50 -mt-[100vh] hidden h-screen w-screen md:block">
-        <Scene />
-        <Bubbles count={300} speed={2.5} repeat={true}  />
-      </View>
-  )}
-      
+        <View className="hero-scene pointer-events-none sticky top-0 z-50 -mt-[100vh] hidden h-screen w-screen md:block">
+          <Scene />
+          <Bubbles count={300} speed={2.5} repeat={true} />
+        </View>
+      )}
+
       <div className="grid">
         <div className="grid h-screen place-items-center">
           <div className="grid auto-rows-min place-items-center text-center">
